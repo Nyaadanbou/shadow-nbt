aae87e70e68a1164210b5f61bcea4d59d4cf60c0
Akiranya <porridgen@gmail.com>
2024-07-25 01:28:14 +0800

    fix Tag#copy() not returning actual shadow type

--- src/main/java/cc/mewcraft/nbt/Tag.java
+++ src/main/java/cc/mewcraft/nbt/Tag.java
@@ -2,6 +2,7 @@ package cc.mewcraft.nbt;
 
 import cc.mewcraft.version.NmsVersion;
 import me.lucko.shadow.Shadow;
+import me.lucko.shadow.ShadowingStrategy;
 import me.lucko.shadow.bukkit.Mapping;
 import me.lucko.shadow.bukkit.NmsClassTarget;
 import me.lucko.shadow.bukkit.ObfuscatedTarget;
@@ -35,6 +36,9 @@ public interface Tag extends Shadow {
             @Mapping(value = "copy", version = NmsVersion.v1_20_R4),
             @Mapping(value = "d", version = NmsVersion.v1_20_R3)
     })
+    @ShadowingStrategy(
+            wrapper = NbtShadowingStrategy.ForShadowTags.class
+    )
     Tag copy();
 
     @ObfuscatedTarget({
